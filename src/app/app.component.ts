02ad33fa5a9c19a516308f685198ae9ba06d1c00
renoth <johannes@renoth.dev>
2024-07-17 23:04:32 +0200

    you can now move sliders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, Input, SimpleChanges} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatCard} from "@angular/material/card";
+import {MatSlider, MatSliderThumb} from "@angular/material/slider";
+import {BattleresultComponent} from "./battleresult/battleresult.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCard, MatSlider, MatSliderThumb, BattleresultComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tpop-battle';
+  @Input() ownSoldiers: number;
+  @Input() enemySoldiers: number;
+
+  constructor() {
+    this.ownSoldiers = 0;
+    this.enemySoldiers = 0;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+  }
 }
---
+++ src/app/battleresult/battleresult.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'battle-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './battleresult.component.html',
+  styleUrl: './battleresult.component.css'
+})
+export class BattleresultComponent {
+
+}
